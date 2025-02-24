c8f527f6d2b65c94b2584eaaf3d5b2860109cac6
pieersx <piers_84@hotmail.com>
2025-02-24 13:20:18 -0500

    Update install.sh

--- .zsh/install.sh
+++ .zsh/install.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-echo "Instalando plugins de Zsh..."
 ZSH_CUSTOM=~/.oh-my-zsh/custom
 
 git clone --depth=1 https://github.com/marlonrichert/zsh-autocomplete $ZSH_CUSTOM/plugins/zsh-autocomplete
@@ -8,5 +7,3 @@ git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM
 git clone --depth=1 https://github.com/zsh-users/zsh-completions $ZSH_CUSTOM/plugins/zsh-completions
 git clone --depth=1 https://github.com/zsh-users/zsh-history-substring-search $ZSH_CUSTOM/plugins/zsh-history-substring-search
 git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-
-echo "Plugins instalados correctamente."
